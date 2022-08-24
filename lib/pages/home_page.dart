482e790e8eee543cfaf25c05d1b2501809b58ff1
smtalha5654 <syedmuhammadtalha800@gmail.com>
2022-08-24 17:52:24 +0500

    Day 7 Completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/models/catalog.dart';
 import 'package:flutter_course/uttils/widgets/drawer.dart';
+import 'package:flutter_course/uttils/widgets/itemwidget.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -8,9 +10,13 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Flutter App"),
       ),
-      body: const Center(
-        child: Text('Welcome'),
-      ),
+      body: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: CatalogModel.items[index],
+            );
+          }),
       drawer: Mydrawer(),
     );
   }
---
+++ lib/uttils/widgets/itemwidget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_course/models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text("\$${item.price}"),
+      ),
+    );
+  }
+}
